40620cc1665027f0e9436221dde6a492c90602ee
Vuducminh1 <vuducminh10102@gmail.com>
2024-07-26 00:01:10 +0700

    response

--- BackEnd/bookingWeb/src/main/java/com/example/bookingWeb/model/Room.java
+++ BackEnd/bookingWeb/src/main/java/com/example/bookingWeb/model/Room.java
@@ -27,10 +27,11 @@ public class Room {
     private BigDecimal roomPrice;
     private boolean isBooked = false;
 
+
     @Lob
     private Blob photo;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy ="room" ,fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room(){
--- BackEnd/bookingWeb/src/main/java/com/example/bookingWeb/repository/RoomRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookingWeb.repository;
-
-public interface RoomRepository {
-}
